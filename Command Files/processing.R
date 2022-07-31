2cac9568c8aff2b4a9b942a288548c415eb7896b
Sneha Verma <sverma1012@gmail.com>
2022-07-30 22:19:32 -0400

    wrangle the original data to create importable datasets.

--- Command Files/processing.R
+++ Command Files/processing.R
@@ -2,3 +2,6 @@
 # Date of Creation: 31 July 2022
 # Purpose of Script: This script will process the data. It details the import, manipulation, and wrangling of the data.
 
+## Import
+# There are three datasets that will be used for this project:
+
