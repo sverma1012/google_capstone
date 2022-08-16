fc03e5f53ab695e58f6ba4134d349c6ae928b314
Sneha Verma <sverma1012@gmail.com>
2022-08-16 18:22:21 -0400

    start analysis script

---
+++ Command Files/share.R
@@ -0,0 +1,12 @@
+# Author: Sneha Verma
+# Date of Creation: 16 August 2022
+# Purpose of Script: Communicate the results of the analysis and
+# relate it to the business problem
+
+
+
+## SHARE: Focus more on people between 85 and 95 kgs.
+# Focus on people who sleep between 300 to 500 mins. 
+# Help people increase steps.
+# Growth opportunity because people are not as active.
+# people get it to motivate themselves to workout
