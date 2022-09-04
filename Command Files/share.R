dab8acabf0301c4016920b98c467016e4a119f52
Sneha Verma <sverma1012@gmail.com>
2022-09-04 18:22:08 -0500

    create viz.

--- Command Files/share.R
+++ Command Files/share.R
@@ -3,6 +3,38 @@
 # Purpose of Script: Communicate the results of the analysis and
 # relate it to the business problem
 
+# The goal of this project is to analyze consumer's use of a competitor
+# for growth opportunities and recommendations for the marketing team.
+
+# Import datasets
+
+activity = read.csv("../Data/Importable Data/dailyActivity_merged.csv")
+sleepActivity = read.csv("../Data/Importable Data/sleepActivity.csv")
+weightActivity = read.csv("../Data/Importable Data/weightActivity.csv")
+
+
+# The following visualizations describe the results from the analysis.
+
+library(ggplot2)
+
+# Weight and Calories
+
+ggplot(data = weightActivity, aes(x=WeightKg_name, y=calories)) +
+  geom_point(color = 'blue') +
+  geom_smooth(method = lm, color = 'black') +
+  ggtitle('Weight v/s Calories') +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, 
+                                  face = 'bold')) +
+  xlab('Weight (Kgs)') +
+  ylab('Calories') + 
+  ylim(1500, 3500)
+
+# This visualization shows that most participants lost approximately
+# 2000 calories. However, there are outliers- participants between
+# the weight of 85 and 95 kgs lost the most calories.
+
+# Sleep and Calories
+
 
 
 ## SHARE: Focus more on people between 85 and 95 kgs.
